c179379f1f86ab0744405e3b5848eccdc066ae2b
MartinSchvartz <39829852+MartinSchvartz@users.noreply.github.com>
2021-08-31 20:09:22 -0300

    Update scanner.c
    Correcion tp

--- scanner.c
+++ scanner.c
@@ -2,25 +2,8 @@
 // Created by martin on 8/10/2021.
 //
 #include "scanner.h"
-void get_token(int caracter){
-
-    while(caracter != FDT){
-        caracter = getchar();
-        if(caracter == SEP) {
-                printf("\nSeparador: %c", (char) caracter);
-        }else {
-
-            if (isspace(caracter) == 0)
-                printf("\nCadena: ");
-            while (caracter != SEP && caracter != FDT && isspace(caracter) == 0) {
-
-                printf("%c", (char) caracter);
-                caracter = getchar();
-            }
-            if (caracter == SEP) {
-                printf("\nSeparador: %c", (char) caracter);
-            }
-        }
-        }
-    printf("\nFin de Texto:");
+int get_token(){
+    int token;
+    token = getchar();
+    return token;
 }
