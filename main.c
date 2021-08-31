4798231850cef4b776780ded8ee320b6c4aacb70
MartinSchvartz <39829852+MartinSchvartz@users.noreply.github.com>
2021-08-31 20:06:20 -0300

    Update main.c
    Correccion del tp

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 
 #include "scanner.h"
 int main(void) {
-    int caracter;
-    get_token(caracter);
+    int token;
+    token = get_token();
+    while (token != FDT){
+            if(token == SEP) {
+                printf("\nSeparador: %c", (char) token);
+            }else {
+
+                if (isspace(token) == 0)
+                    printf("\nCadena: ");
+                while (token != SEP && token != FDT && isspace(token) == 0) {
+
+                    printf("%c", (char) token);
+                    token = get_token();
+                }
+                if (token == SEP) {
+                    printf("\nSeparador: %c", (char) token);
+                }
+            }
+
+        token = get_token();
+    }
+    printf("\nFin de texto: ");
+
     return 0;
 }
+
