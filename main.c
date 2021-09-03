63976292dfe2d08e238355ef0336d60a10ba542a
MartinSchvartz <39829852+MartinSchvartz@users.noreply.github.com>
2021-09-02 21:30:39 -0300

    Correccion final

--- main.c
+++ main.c
@@ -1,27 +1,20 @@
 #include "scanner.h"
-
+char lexemaCorrespondiente[100];
 int main(void) {
     int token;
+    char lexema[100] = {'\0'};
     token = get_token();
     while (token != FIN_DE_TEXTO){
-        if(token == SEPARADOR) {
-                printf("\nSeparador: %c", (char) token);
-            }else {
-                if (!isspace(token))
-                    printf("\nCadena: ");
-                while (token != SEPARADOR && token != FIN_DE_TEXTO && !isspace(token)) {
-
-                    printf("%c", (char) token);
-                    token = get_token();
-                }
-                if (token == SEPARADOR) {
-                    printf("\nSeparador: %c", (char) token);
-                }
-            }
-
+        switch (token) {
+            case SEPARADOR:
+                printf("\nSeparador: %s", lexemaCorrespondiente);
+                break;
+            case CADENA:
+                printf("\nCadena: %s", lexemaCorrespondiente);
+                break;
+        }
         token = get_token();
     }
     printf("\nFin de texto: ");
-
     return 0;
 }
