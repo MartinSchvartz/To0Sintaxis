4c419c153cbb21a2bd99037cce86c3d691ad0ef2
MartinSchvartz <39829852+MartinSchvartz@users.noreply.github.com>
2021-09-03 10:10:26 -0300

    Update scanner.h

--- scanner.h
+++ scanner.h
@@ -5,7 +5,7 @@
 // Created by martin on 8/10/2021.
 //
 enum TokenType{FIN_DE_TEXTO,SEPARADOR,CADENA};
-extern char lexemaCorrespondiente[100];
+char lexemaCorrespondiente[100];
 #ifndef TP1PRO_SCANNER_H
 #define TP1PRO_SCANNER_H
 int get_token();
